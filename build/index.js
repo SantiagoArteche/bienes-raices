efc2fe77a08a40a1d991491e4425d11da25ad25e
santiago arteche <santiarteche@hotmail.com>
2024-04-10 18:47:24 -0300

    first commit

---
+++ build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+function greeter(person) {
+    return "Hello, " + person;
+}
+let user = "Jane User";
+let blog = 3;
+console.log("asd");
+console.log(blog);
+console.log(blog);
+console.log(blog);
+console.log(user);
+console.log("hola que tal");
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+function greeter(person: number): any {
+  return "Hello, " + person;
+}
+
+let user = "Jane User";
+let blog: number = 3;
+
+console.log("asd");
+console.log(blog);
+console.log(blog);
+console.log(blog);
+console.log(user);
+console.log("hola que tal");
